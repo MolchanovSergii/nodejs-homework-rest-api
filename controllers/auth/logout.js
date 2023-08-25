cd23bc507aaf722bfeea66fd4cced5ddde6c60ec
MolchanovSergii <molchanse@gmail.com>
2023-08-26 01:43:04 +0300

    summury fix

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -4,7 +4,9 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.status(204);
+  res.status(204).json({
+    message: "Logout success",
+  });
 };
 
 module.exports = logout;
